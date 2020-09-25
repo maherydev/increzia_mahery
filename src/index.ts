ee07dddf5aeb0aa66eb92d770e12e9c4970c00a1
MaheryDev <67246250+maherydev@users.noreply.github.com>
2020-09-25 13:03:06 +0200

    Mahery_test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,46 @@
-const text: string = 'test!!!'
-console.log(text);
+console.log('EXERCICE 1');
+function matrice(monTableau){
+  monTableau = new Array(3);
+  for (var i = 0; i < 4; i++)
+  for (var j = 0 ; j<3; j++)
+  for (var k = 0 ; k<3; k++)
+  {
+ monTableau[i,j,k] = new Array(i);
+monTableau[i,j,k]=[i-3,j-1,k];
+
+  }
+  console.log(monTableau)
+}
+matrice()
+
+console.log('EXERCICE 2');
+
+var array = [0,3,-5,12,-18,20];
+
+var sum = array.reduce(function(a, b){
+
+    return a + b;
+
+}, 0);
+console.log(sum);
+
+
+console.log('EXERCIE 2.2')
+function renverser(){
+  var monTableau = [0,3,-5,12,-18,-20];
+  monTableau.reverse();
+  console.log(monTableau) 
+  return monTableau
+}
+renverser()
+
+
+console.log('exo 3.1 : D')
+
+console.log('exo 3.2 : A')
+console.log('exo 3.3 : A')
+console.log('exo 3.2 : carte manquante = 3 de trefle')
+
+
+
+
